fd8540d2e6226920226dfb27e586eac6163e0e42
BARAGAA <moh0916ali@gmail.com>
2024-03-17 17:31:30 +0100

    Jenkins Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.fizzBuzz.FizzBuzz;
+
 public class Main {
+    private FizzBuzz fizzBuzz = new FizzBuzz();
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(FizzBuzz.convert(15));
     }
 }
